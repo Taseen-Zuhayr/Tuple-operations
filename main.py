e77ef559c2f3f7b44189a3c401841172756f3395
Taseen-Zuhayr <taseen.zuhayr@gmail.com>
2024-11-15 22:21:52 +0600

    Some cool tuple operations

---
+++ main.py
@@ -0,0 +1,17 @@
+#Tuple with different datatypes
+tuple1 = (43235,37643,4763,24222265,65)
+print(tuple1)
+
+#Adding 9
+tuple1 += (45,)
+print(tuple1)
+
+
+#counting 30
+tuple2 = (45,2135,30,343,65,30,34,20,30)
+print(tuple2.count(30))
+
+#slicing
+tuple3 = (212,42,325,58564,745366,1241,4525263,4763,643)
+slice = tuple3[2:6]
+print(slice)
